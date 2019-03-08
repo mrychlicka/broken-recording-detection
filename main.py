caa7cff86cb654f2776e1eb76a68675cafe65483
mrychlicka <rychlickam@gmail.com>
2019-03-08 17:27:23 +0100

    ref: Refactored main.py

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ from pandas import DataFrame
 
 def draw_plot(audio_file):
     """
-    Draw a plot of audio file
-    :param audio_file: String - path to audio file. Should be .wav
-    :return: Plot of audio file (x - time, y - dBFs)
+    Draw a signal as a plot
+    :param audio_file: String - path to audio file (.wav)
+    :return: Return signal plot (x - time, y - amplitude)
     """
     spf = wave.open(audio_file, 'r')
-    signal = spf.readframes(-1)
-    signal = np.fromstring(signal, 'Int16')
+    signal = np.fromstring(spf.readframes(-1), 'Int16')
     plt.figure(1)
     plt.plot(signal)
     return plt.show()
@@ -22,9 +21,9 @@ def draw_plot(audio_file):
 
 def get_audio_total_sample(audio_file):
     """
-    Get basic information about audio file
-    :param audio_file: String - path to audio file. Should be .wav
-    :return: Return dict with pair of information name and value
+    Get audio file total sample amount
+    :param audio_file: String - path to audio file (.wav)
+    :return: Return total sample amount in audio file
     """
     f = sf.SoundFile(audio_file)
     return len(f)
@@ -32,71 +31,70 @@ def get_audio_total_sample(audio_file):
 
 def get_audio_sample_rate(audio_file):
     """
-    Get basic information about audio file
-    :param audio_file: String - path to audio file. Should be .wav
-    :return: Return dict with pair of information name and value
+    Get sample rate
+    :param audio_file: String - path to audio file (.wav)
+    :return: Return sample rate
     """
     f = sf.SoundFile(audio_file)
     return f.samplerate
 
 
 def put_signal_to_dataframe(audio_file):
+    """
+	Present audio signal as a dataframe
+	:param audio_file: String - path to audio file (.wav)
+	:return: Return dataframe with two columns: amplitude and sample number
+	"""
     spf = wave.open(audio_file, 'r')
-    signal = spf.readframes(-1)
-    signal = np.fromstring(signal, 'Int16')
+    signal = np.fromstring(spf.readframes(-1), 'Int16')
     df = DataFrame(signal)
     df['sample'] = [i for i in range(get_audio_total_sample(audio_file=audio_file))]
     return df
 
 
 FILENAME = sys.argv[1]
+# FILENAME = '/home/gosia/python/broken_recording_detection/nan-ai-file-2.wav'
+SAMPLE_RATE = get_audio_sample_rate(audio_file=FILENAME)
+FROM_SILENCE_TO_NOISE = 20
+MIN_SAMPLES_NUMBER_IN_GAP = 130
 
 draw_plot(audio_file=FILENAME)
-sample_rate = get_audio_sample_rate(audio_file=FILENAME)
-
-
 dataframe = put_signal_to_dataframe(audio_file=FILENAME)
-# print(dataframe)
 
-# odciecie poczatku pliku
-start_silence = 0
+start_silence = 0  # eliminate audio start silence
 start_signal_index = 0
 for sample_ampli in dataframe[0]:
 	start_signal_index += 1
-	if abs(sample_ampli) > 20:  # TODO: nie hardkodowac
+	if abs(sample_ampli) > FROM_SILENCE_TO_NOISE:
 		start_silence += 1
 	if start_silence > 50:
 		break
 
-print("start_silence: ", start_silence)
-print("start_signal_index: ", start_signal_index)
-
-# odciecie konca pliku
-end_silence = 0
+end_silence = 0  # eliminate audio end silence
 end_signal_index = get_audio_total_sample(FILENAME)
 for sample_ampli in reversed(dataframe[0]):
 	end_signal_index -= 1
-	if abs(sample_ampli) > 20:  # TODO: nie hardkodowac
+	if abs(sample_ampli) > FROM_SILENCE_TO_NOISE:
 		end_silence += 1
 	if end_silence > 50:
 		break
 
-print("end_silence: ", end_silence)
-print("end_signal_index: ", end_signal_index)
-
-# szukanie dziury
-ile_sampli = 130
 start_gap_sample = 0
 gap_length = 0
+invalid = False
 for idx, sample_ampli in enumerate(dataframe[0]):
 	if idx >= start_signal_index and idx <= end_signal_index:
 		if abs(sample_ampli) <= 1:
 			start_gap_sample = idx
-			print("start_gap_sample: ", start_gap_sample)
+			# logging.info("start_gap_sample: ", start_gap_sample)
 			gap_length += 1
 		else:
 			gap_length = 0
 
-		if gap_length >= ile_sampli:
-			print("there is a gap, start sample: ", start_gap_sample - ile_sampli)
+		if gap_length >= MIN_SAMPLES_NUMBER_IN_GAP:
+			invalid = True
+			print("{} {} {}".format(FILENAME, 'invalid', start_gap_sample - MIN_SAMPLES_NUMBER_IN_GAP))
 			break
+
+if not invalid:
+	print('{} {}'.format(FILENAME, 'valid'))
